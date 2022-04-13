b16e9a5584b1e50a53fbdd3303edfaa9968525b4
AzharAlShaleh <azharsh291@gmail.com>
2022-04-13 13:14:54 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace azhar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
